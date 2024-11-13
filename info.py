a5433ccbc44661715d7edd3c31badcfbeac44430
JadejaShu <139604204+JadejaShu@users.noreply.github.com>
2024-11-13 18:34:01 +0530

    bug fix

--- info.py
+++ info.py
@@ -35,7 +35,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URL = environ.get('DATABASE_URL', "")
+DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://Nataliermvzz:QOiuh213MSIK@natalie.if4691m.mongodb.net/")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Natalie")
 FILE_DB_URL = environ.get("FILE_DB_URL", DATABASE_URL)
 FILE_DB_NAME = environ.get("FILE_DB_NAME", DATABASE_NAME)
